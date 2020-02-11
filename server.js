6b929ae9b675acb547453215d21595c9fd125087
imjvictor98 <jv.camara@hotmail.com>
2020-02-11 11:50:54 -0300

    Publish

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const server = express();
 
 server.use(express.json());
+server.use(cors);
 
 server.use("/", routes);
 
-server.listen(8080, () => {
+server.listen(3000, () => {
   console.log("NodeJs is listening your fairytales in port 8080");
 });
